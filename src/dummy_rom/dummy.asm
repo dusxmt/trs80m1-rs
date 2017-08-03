8c97294b73c68ca0a8cdc80e0f58e60d6b575931
Marek Benc <dusxmt@gmx.com>
2017-08-03 06:48:37 +0200

    Modified the dummy rom to use the block transfer instruction ldir, and to use an explicit halt instruction.

--- src/dummy_rom/dummy.asm
+++ src/dummy_rom/dummy.asm
@@ -22,21 +22,11 @@ video_mem	equ	0x3C00
 		di
 
 ; Copy the new screen content into video memory:
-		ld	hl, video_mem
-		ld	de, message
-		ld	b, 16		; Outer loop iteration count.
-outer_loop:	ld	c, 64		; Inner loop iteration count.
-inner_loop:	ld	a, (de)
-		ld	(hl), a
-		inc	de
-		inc	hl
-		dec	c
-		jp	nz, inner_loop	; Loop until we underflow.
-		dec	b
-		jp	nz, outer_loop	; Same for the outer loop.
-
-stuck:		jp	stuck
-
+start:		ld	hl, message
+		ld	de, video_mem
+		ld	bc, 16 * 64
+		ldir
+		halt
 
 ; The message to display on the screen:
 message:	incbin "message.bin"
